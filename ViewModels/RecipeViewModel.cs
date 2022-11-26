2c95db566be39fbec46b7228f5f864ade67f7293
PeterCellar <petocell@gmail.com>
2022-11-26 20:52:00 +0100

    predpripraveny viewModel pre recept

--- ViewModels/RecipeViewModel.cs
+++ ViewModels/RecipeViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using yummyCook.Models;
 
 namespace yummyCook.ViewModels
 {
-    public class RecipeViewModel
+    public class RecipeViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private RecipeModel _recipe;
 
+        public RecipeModel Recipe
+        {
+            get => _recipe;
+            set { _recipe = value; OnPropertyChanged(); }
+        }
+
+        public RecipeViewModel() { } // TODO
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
 }
