70da25c82db7716900d7fdc71f4da34e9bde86db
PeterCellar <petocell@gmail.com>
2022-11-26 20:38:09 +0100

    pridany model receptu spolu s enumami

---
+++ Models/Enums/CuisineType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yummyCook.Models.Enums
+{
+    public enum CuisineType
+    {
+        Slovak,
+        Czech,
+        Indian,
+        Mediterranean,
+        British,
+        Mexican
+    }
+}
--- Models/RecipeModel.cs
+++ Models/RecipeModel.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using yummyCook.Models.Enums;
 
 namespace yummyCook.Models
 {
     public class RecipeModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string RecipeProcedure { get; set; }
+        public string Rating { get; set; }
+        public TimeSpan CookingTime { get; set; }
+        public FoodType FoodType { get; set; }
+        public CuisineType CuisineType { get; set; }
+        public ObservableCollection<string> KitchenAppliances { get; set; }
+        public ObservableCollection<Diet> Diets { get; set; }
+        public ObservableCollection<Allergen> Allergens { get; set; }
+        public ObservableCollection<string> SelectedIngredients { get; set; }
+        public ObservableCollection<string> Pictures { get; set; }
+        public bool Favorite { get; set; }
+
     }
 }
